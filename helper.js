67af4edfbad546710c706f278e315d9fa906e083
darkhorsedev1997 <dina.simashkevych@gmail.com>
2023-06-04 00:00:00 +0300

    Fixed bug in user session management

--- helper.js
+++ helper.js
@@ -1,34 +1,4 @@
 Contribution: 2023-06-04 00:00
 
-Contribution: 2023-06-05 00:00
-
-Contribution: 2023-06-06 00:00
-
-Contribution: 2023-06-07 00:00
-
-Contribution: 2023-06-08 00:00
-
-Contribution: 2023-06-09 00:00
-
-Contribution: 2023-06-10 00:00
-
-Contribution: 2023-06-11 00:00
-
-Contribution: 2023-06-12 00:00
-
-Contribution: 2023-06-13 00:00
-
-Contribution: 2023-06-14 00:00
-
-Contribution: 2023-06-15 00:00
-
-Contribution: 2023-06-16 00:00
-
-Contribution: 2023-06-17 00:00
-
-Contribution: 2023-06-18 00:00
-
-Contribution: 2023-06-19 00:00
-
-Contribution: 2023-06-20 00:00
+Contribution: 2023-06-04 00:00
 
